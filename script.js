28302a5a4a47aa6f7a04dac657b7958c79f74d69
ChipChaddleson <rich.a.denton@gmail.com>
2022-02-19 16:18:19 -0500

    sarinas merge

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 
 let btn = document.getElementById("cta-btn");
-let overlay = document.getElementById("cta-btn");
+// let overlay = document.getElementById("cta-btn");
 btn.addEventListener('click', () => {
     // rediredt to https://www.unicef.ca/en/search?search= + document.getElementById("questions").value
     window.location.href = "https://www.unicef.ca/en/search?search=" + document.getElementById("questions").value;
 })
+
